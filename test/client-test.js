44682e3d422819b5ce26b8aed78b6af21a879242
Erik Straub <erik.straub@oddnetworks.com>
2016-05-16 11:43:38 -0400

    Refactors & adds client tests

--- test/client-test.js
+++ test/client-test.js
@@ -4,7 +4,7 @@ const Promise = require('bluebird');
 const test = require('tape');
 const sinon = require('sinon');
 const msgpack = require('oddcast-msgpack');
-const tcpTransport = require('../lib/transport').create({client: {host: '127.0.0.1', port: 1544}});
+const oddcastTCPTransport = require('../lib/transport');
 
 const payload = {first: 1};
 const pattern = {role: 'test'};
@@ -17,17 +17,17 @@ const response = {val: 'TCP-Response'};
 
 	server.listen(1544, '127.0.0.1');
 
-	const subject = tcpTransport;
+	const subject = oddcastTCPTransport.create({client: {host: '127.0.0.1', port: 1544}});
 
 	const messageSentHandler = sinon.spy();
 	const responseHandler = sinon.spy();
 	const errorHandler = sinon.spy();
 
-	test('before all sendWithNoError', function (t) {
+	test('before all sendWithNoError', t => {
 		subject.on('message:sent', messageSentHandler);
-		subject.on('error', errorHandler);
+		subject.on('client:error', errorHandler);
 
-		subject.on('client:connect', function () {
+		subject.on('client:connect', () => {
 			subject.write({
 				pattern: pattern,
 				payload: payload
@@ -39,18 +39,57 @@ const response = {val: 'TCP-Response'};
 		subject.resume();
 	});
 
-	test('write() is called', function (t) {
-		t.plan(4);
-		const options = messageSentHandler.args[0][0];
-		t.deepEqual(options.pattern, pattern, 'pattern is set');
-		t.deepEqual(options.payload, payload, 'payload is set');
+	test('write() is called', t => {
+		t.plan(5);
+		t.equal(messageSentHandler.callCount, 1, 'message:sent emitted once');
+		const sentMessage = messageSentHandler.args[0][0];
+		t.deepEqual(sentMessage.pattern, pattern, 'pattern is set');
+		t.deepEqual(sentMessage.payload, payload, 'payload is set');
 		t.deepEqual(responseHandler.args[0][0], response, 'response received from server');
-		t.equal(errorHandler.callCount, 0, 'error is not emitted');
-		t.end();
+		t.equal(errorHandler.callCount, 0, 'no client:error emitted');
 	});
 
-	test('after all', function (t) {
-		subject.client.destroy();
+	test('after all', t => {
+		subject.close();
+		server.close(t.end);
+	});
+})();
+
+(function sendWithError() {
+	const server = net.createServer();
+
+	server.listen(1545, '127.0.0.1');
+
+	const subject = oddcastTCPTransport.create({client: {host: '127.0.0.1', port: 1545}});
+
+	const messageSentHandler = sinon.spy();
+	const responseHandler = sinon.spy();
+	const errorHandler = sinon.spy();
+	const clientErrorListener = sinon.spy();
+
+	test('before all sendWithNoError', t => {
+		subject.on('message:sent', messageSentHandler);
+		subject.on('client:error', errorHandler);
+		subject.client.on('error', clientErrorListener);
+
+		subject.on('client:connect', () => {
+			subject.write('{{}}').then(responseHandler);
+
+			Promise.delay(500).then(t.end);
+		});
+
+		subject.resume();
+	});
+
+	test('write() is called', t => {
+		t.plan(3);
+		t.equal(messageSentHandler.callCount, 0, 'transport did not emit "message:sent"');
+		t.equal(clientErrorListener.callCount, 1, 'client emitted "error"');
+		t.equal(errorHandler.callCount, 1, 'transport emitted "client:error"');
+	});
+
+	test('after all', t => {
+		subject.close();
 		server.close(t.end);
 	});
 })();
